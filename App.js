542ac87e13b0cdf83ab6f673e7e2ffac4d16bd4a
Sarthak Bhojane <105963807+sarthak-bhojane@users.noreply.github.com>
2026-03-31 11:00:17 +0530

    AI update

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import './App.css';
-function App() {
- return (
- <div className='calculator'>
- <Display />
- <Buttons />
- </div>
- );
- }
- export default App;
+const App = () => {
+  return (
+    <div>Hello World</div>
+  );
+};
+export default App;
